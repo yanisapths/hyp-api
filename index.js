6437755b69ed6f6202d7891cd5486e8271ca3fc3
yanisapths <yanisa_poongthaisong@cmu.ac.th>
2022-12-18 13:51:57 +0700

    use cors

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ require("dotenv").config({ path: "config.env" });
 const mongoose = require("mongoose");
 const express = require("express");
 var cors = require("cors");
+const corsOptions ={
+  origin:'http://localhost:3000', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 const bodyParser = require("body-parser");
 const awsServerlessExpress = require("aws-serverless-express");
 
@@ -20,14 +26,13 @@ app.use(require("./server/routes/review"));
 const db = require("./server/db/conn");
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Global error handling
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.header("Access-Control-Allow-Origin", "*, http://localhost:3000, https://daycare-portal.vercel.app");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Credentials", true);
   res.header(
     "Access-Control-Allow-Headers",

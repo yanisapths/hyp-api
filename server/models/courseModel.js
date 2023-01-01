709fc0c05400058ecc5b1a251126432e61c8d53a
yanisapths <yanisa_poongthaisong@cmu.ac.th>
2023-01-01 10:53:52 +0700

    add course

---
+++ server/models/courseModel.js
@@ -0,0 +1,46 @@
+const {Schema,model} = require("mongoose");
+const uuid = require("uuid");
+
+const ProcedureSchema = mongoose.Schema({
+    procedureName: {
+      type: String,
+      required: false,
+    },
+    price: {
+      type: String,
+      required: false,
+    },
+  });
+
+const CourseSchema = new Schema({
+  course_id: {
+    type: String,
+    default: function genUUID() {
+      return uuid.v1();
+    },
+    required: true,
+  },
+  courseName: {
+    type: String,
+    required: false,
+  },
+  amount: {
+    type: String,
+    required: false,
+  },
+  duration: {
+    type: String,
+    required: false,
+  },
+  totalPrice: {
+    type: String,
+    required: false,
+  },
+  procedures: [ProcedureSchema],
+  owner_id: { type: String, ref: "Clinic"},
+  clinic_id: { type: Schema.ObjectId, ref: "Clinic" },
+});
+
+const Course = model("Course", CourseSchema);
+
+module.exports = { Course };

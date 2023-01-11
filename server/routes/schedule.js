b6290cd60ef962aa0e1e582849599c2cbfc646f9
yanisapths <yanisa_poongthaisong@cmu.ac.th>
2023-01-11 16:44:55 +0700

    fig bug

--- server/routes/schedule.js
+++ server/routes/schedule.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { Schedule } = require("../models/Schedule");
+const { Schedule } = require("../models/ScheduleModel");
 const mongoose = require("mongoose");
 const toId = mongoose.Types.ObjectId;
 mongoose.set("strictQuery", false);
@@ -29,39 +29,35 @@ scheduleRoutes.route("/schedule").get(async (req, res) => {
 });
 
 // Get schedule by id
-scheduleRoutes
-  .route("/schedule/:schedule_id")
-  .get(async (req, res) => {
-    const dbConnect = db.getDb();
-    try {
-      const schedule = await dbConnect
-        .collection("scheduleSlot")
-        .findOne(toId(req.params.schedule_id));
-      res.send(schedule);
-    } catch {
-      res.status(404);
-      res.send({ error: "schedule doesn't exist!" });
-    }
-  });
+scheduleRoutes.route("/schedule/:schedule_id").get(async (req, res) => {
+  const dbConnect = db.getDb();
+  try {
+    const schedule = await dbConnect
+      .collection("scheduleSlot")
+      .findOne(toId(req.params.schedule_id));
+    res.send(schedule);
+  } catch {
+    res.status(404);
+    res.send({ error: "schedule doesn't exist!" });
+  }
+});
 
 // Get Appointment by clinic id
-scheduleRoutes
-  .route("/schedule/match/:clinic_id")
-  .get(async (req, res) => {
-    const dbConnect = db.getDb();
-    const clinicId = toId(req.params.clinic_id);
-    try {
-      await dbConnect
-        .collection("scheduleSlot")
-        .aggregate([{ $match: { clinic_id: new ObjectID(clinicId) } }])
-        .toArray((err, result) => {
-          res.send(result);
-        });
-    } catch {
-      res.status(404);
-      res.send({ error: "Failed to fetch clinic's schedule" });
-    }
-  });
+scheduleRoutes.route("/schedule/match/:clinic_id").get(async (req, res) => {
+  const dbConnect = db.getDb();
+  const clinicId = toId(req.params.clinic_id);
+  try {
+    await dbConnect
+      .collection("scheduleSlot")
+      .aggregate([{ $match: { clinic_id: new ObjectID(clinicId) } }])
+      .toArray((err, result) => {
+        res.send(result);
+      });
+  } catch {
+    res.status(404);
+    res.send({ error: "Failed to fetch clinic's schedule" });
+  }
+});
 
 // Get schedule by clinic owner's session
 scheduleRoutes
@@ -83,25 +79,21 @@ scheduleRoutes
   });
 
 // This section will help you create a new document.
-scheduleRoutes
-  .route("/schedule/create/:clinic_id")
-  .post(async (req, res) => {
-    const dbConnect = db.getDb();
-    const clinicId = toId(req.params.clinic_id);
-    const create = await Schedule.create({
-        ...req.body,
-      clinic_id: clinicId,
-    });
-    dbConnect
-      .collection("scheduleSlot")
-      .insertOne(create, (err, result) => {
-        if (err) {
-          res.status(400).send("Error inserting schedule!");
-        } else {
-          return res.status(201).json(create);
-        }
-      });
+scheduleRoutes.route("/schedule/create/:clinic_id").post(async (req, res) => {
+  const dbConnect = db.getDb();
+  const clinicId = toId(req.params.clinic_id);
+  const create = await Schedule.create({
+    ...req.body,
+    clinic_id: clinicId,
   });
+  dbConnect.collection("scheduleSlot").insertOne(create, (err, result) => {
+    if (err) {
+      res.status(400).send("Error inserting schedule!");
+    } else {
+      return res.status(201).json(create);
+    }
+  });
+});
 
 // This section will help you update a document by id.
 scheduleRoutes.route("/schedule/update/:id").put(async (req, res) => {
@@ -109,7 +101,7 @@ scheduleRoutes.route("/schedule/update/:id").put(async (req, res) => {
   const scheduleId = toId(req.params.id);
   const updates = {
     $set: {
-      ...req.body
+      ...req.body,
     },
   };
   await dbConnect
@@ -128,9 +120,7 @@ scheduleRoutes.route("/schedule/delete/:id").delete(async (req, res) => {
   const dbConnect = db.getDb();
   const scheduleId = toId(req.params.id);
   try {
-    await dbConnect
-      .collection("scheduleSlot")
-      .deleteOne({ _id: scheduleId });
+    await dbConnect.collection("scheduleSlot").deleteOne({ _id: scheduleId });
     res.status(200).send("Schedule has been deleted!");
   } catch {
     res.status(404).send({ error: "Schedule doesn't exist!" });

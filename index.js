210f7b017fe1e0dca901ff3706c35a00d3f60ef2
yanisapths <yanisa_poongthaisong@cmu.ac.th>
2023-01-26 17:21:32 +0700

    refactor: staff model, routes

--- index.js
+++ index.js
@@ -4,12 +4,11 @@ const mongoose = require("mongoose");
 const express = require("express");
 var cors = require("cors");
 const bodyParser = require("body-parser");
-const awsServerlessExpress = require("aws-serverless-express");
-
 var app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use(express.static('public'));
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true, parameterLimit:50000}));
 
--- server/routes/staff.js
+++ server/routes/staff.js
@@ -3,12 +3,40 @@ const { Staff } = require("../models/staffModel");
 const mongoose = require("mongoose");
 const toId = mongoose.Types.ObjectId;
 mongoose.set("strictQuery", false);
+const multer = require("multer");
+const DIR = "./public/";
+const db = require("../db/conn");
+const { ObjectID } = require("bson");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, DIR);
+  },
+  name: (req, file, cb) => {
+    const fileName = file.name.toLowerCase().split(" ").join("-");
+    cb(null, filenameName);
+  },
+});
+
+var upload = multer({
+  storage: storage,
+  fileFilter: (req, file, cb) => {
+    if (
+      file.mimetype == "image/png" ||
+      file.mimetype == "image/jpg" ||
+      file.mimetype == "image/jpeg"
+    ) {
+      cb(null, true);
+    } else {
+      cb(null, false);
+      return cb(new Error("Only .png, .jpg and .jpeg format allowed!"));
+    }
+  },
+});
 
 // staffRoutes is an instance of the express router.
 const staffRoutes = express.Router();
 // This will help us connect to the database
-const db = require("../db/conn");
-const { ObjectID } = require("bson");
 
 // Read
 // This section will help you get a list of all the documents.
@@ -29,63 +57,70 @@ staffRoutes.route("/staff").get(async (req, res) => {
 });
 
 // Get Staff by id
-staffRoutes
-  .route("/staff/:staff_id")
-  .get(async (req, res) => {
-    const dbConnect = db.getDb();
-    try {
-      const staff = await dbConnect
-        .collection("clinicStaffDetails")
-        .findOne(toId(req.params.staff_id));
-      res.send(staff);
-    } catch {
-      res.status(404);
-      res.send({ error: "Staff doesn't exist!" });
-    }
-  });
+staffRoutes.route("/staff/:staff_id").get(async (req, res) => {
+  const dbConnect = db.getDb();
+  try {
+    const staff = await dbConnect
+      .collection("clinicStaffDetails")
+      .findOne(toId(req.params.staff_id));
+    res.send(staff);
+  } catch {
+    res.status(404);
+    res.send({ error: "Staff doesn't exist!" });
+  }
+});
 
-  // Get staff by clinic id
-staffRoutes
-.route("/staff/match/:clinic_id")
-.get(async (req, res) => {
+// Get staff by clinic id
+staffRoutes.route("/staff/match/:clinic_id").get(async (req, res) => {
   const dbConnect = db.getDb();
   const clinicId = toId(req.params.clinic_id);
   try {
     await dbConnect
       .collection("clinicStaffDetails")
-      .aggregate([
-        { $match: { 'clinic_id': new ObjectID(clinicId)} 
-      }
-    ]).toArray( (err,result)=> {
-      res.send(result);
-    })
-
+      .aggregate([{ $match: { clinic_id: new ObjectID(clinicId) } }])
+      .toArray((err, result) => {
+        res.send(result);
+      });
   } catch {
     res.status(404);
-    res.send({ error: "Failed to fetch clinic's staff"});
+    res.send({ error: "Failed to fetch clinic's staff" });
   }
 });
 
-// This section will help you create a new document.
-staffRoutes.route("/staff/create/:clinic_id").post(async (req, res) => {
+// Get staff by owner id
+staffRoutes.route("/staff/owner/:owner_id").get(async (req, res) => {
   const dbConnect = db.getDb();
-  const clinicId = toId(req.params.clinic_id);
+  try {
+    await dbConnect
+      .collection("clinicStaffDetails")
+      .aggregate([{ $match: { owner_id: req.params.owner_id } }])
+      .toArray((err, result) => {
+        res.send(result);
+      });
+  } catch {
+    res.status(404);
+    res.send({ error: "Failed to fetch clinic's staff" });
+  }
+});
+
+
+// This section will help you create a new document.
+staffRoutes.route("/staff/create/:owner_id").post(upload.single("staffImage"),async (req, res) => {
+  const url = req.protocol + "://" + req.get("host");
+  const dbConnect = db.getDb();
   const create = await Staff.create({
-    staffName: req.body.staffName,
-    phoneNumber: req.body.phoneNumber,
-    position: req.body.position,
-    expertArea: req.body.expertArea,
-    professionalLicense: req.body.professionalLicense,
-    staffImageUrl: req.body.staffImageUrl,
-    clinic_id: clinicId
+    ...req.body,
+    staffImage: url + "/public/" + req.body.staffImage,
   });
-  dbConnect.collection("clinicStaffDetails").insertOne(create, (err, result) => {
-    if (err) {
-      res.status(400).send("Error inserting staff!");
-    } else {
-      return res.status(201).json(create);
-    }
-  });
+  dbConnect
+    .collection("clinicStaffDetails")
+    .insertOne(create, (err, result) => {
+      if (err) {
+        res.status(400).send("Error inserting staff!");
+      } else {
+        return res.status(201).json(create);
+      }
+    });
 });
 
 // This section will help you update a document by id.
@@ -94,7 +129,7 @@ staffRoutes.route("/staff/update/:id").put(async (req, res) => {
   const staffId = toId(req.params.id);
   const updates = {
     $set: {
-      ...req.body
+      ...req.body,
     },
   };
   await dbConnect

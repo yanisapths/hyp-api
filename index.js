7bdf0325962590f7e56ab5212679638326d79ba2
yanisapths <yanisa_poongthaisong@cmu.ac.th>
2023-03-02 17:56:36 +0700

    fix: header

--- index.js
+++ index.js
@@ -31,9 +31,8 @@ const PORT = process.env.PORT || 5000;
 // Global error handling
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.setHeader('Access-Control-Allow-Credentials', true)
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
